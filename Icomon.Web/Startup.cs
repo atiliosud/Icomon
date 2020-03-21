a238a1045a5751f31addbb05732a023accc29c06
Atilio <atiliosud@gmail.com>
2020-03-21 18:51:25 -0300

    Atualizações

--- Icomon.Web/Startup.cs
+++ Icomon.Web/Startup.cs
@@ -1,10 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Icomon.Infra.Data.Context;
+using Icomon.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -20,10 +25,27 @@ namespace Icomon.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseInMemoryDatabase("IcomonDatabase");
+                options.ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            services.AddControllersWithViews();
+
+            services.AddMemoryCache();
+
             services.AddRazorPages();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
+            // Inject Dependencies.
+            DIContainer.RegisterDependencies(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
